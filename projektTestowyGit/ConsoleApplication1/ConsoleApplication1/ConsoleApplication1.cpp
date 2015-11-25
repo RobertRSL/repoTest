d7cb6e35f9fee750baab293e9cd239b2fd2a0638
RobertRSl <r.chalaszczyk@gmail.com>
2015-11-25 11:11:19 +0100

    pierwszy commit

---
+++ projektTestowyGit/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Program dziala prawidlowo" << endl;
+	return 0;
+}
+
